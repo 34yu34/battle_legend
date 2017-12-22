c81cc66a49e80a23a35b1893aa7c4b12d6abd591
Billy <billy.bouchard03@gmail.com>
2017-12-22 18:45:58 -0500

    Add esc key to quit

---
+++ lib/include/keyboard.h
@@ -0,0 +1,12 @@
+#ifndef KEYBOARD_H
+#define KEYBOARD_H
+
+#include "GL/freeglut.h"
+#include "GL/gl.h"
+
+namespace Keyboard
+{
+  void keyboard(unsigned char key, int x, int y);
+}
+
+#endif
---
+++ lib/source/keyboard.cpp
@@ -0,0 +1,17 @@
+#include "keyboard.h"
+
+
+namespace Keyboard
+{
+  /*********************************************************************
+  * Comment
+  *********************************************************************/
+  void keyboard(unsigned char key, int x, int y)
+  {
+    switch (key) {
+    case 27 : /* ASCII code of the ESC key */
+      exit(0);
+      break;
+    }
+  }
+}
